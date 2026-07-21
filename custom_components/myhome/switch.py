9dac9f8a680ecd9f7424e7f04ee81942cfca9b16
Dav41K9 <d.quattrone@hotmail.com>
2026-07-21 14:32:14 +0200

    Update switch.py

--- custom_components/myhome/switch.py
+++ custom_components/myhome/switch.py
@@ -1,163 +1,83 @@
-"""Support for MyHome switches (light modules used for controlled outlets, relays)."""
-from homeassistant.components.switch import (
-    DOMAIN as PLATFORM,
-    SwitchDeviceClass,
-    SwitchEntity,
-)
-from homeassistant.const import (
-    CONF_NAME,
-    CONF_MAC,
-)
+"""Switch platform for BTicino MyHOME."""
 
-from OWNd.message import (
-    OWNLightingEvent,
-    OWNLightingCommand,
-)
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from homeassistant.components.switch import SwitchDeviceClass, SwitchEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import (
-    CONF_PLATFORMS,
-    CONF_ENTITY,
-    CONF_ENTITY_NAME,
-    CONF_ICON,
-    CONF_ICON_ON,
-    CONF_WHO,
-    CONF_WHERE,
-    CONF_BUS_INTERFACE,
-    CONF_MANUFACTURER,
-    CONF_DEVICE_MODEL,
     CONF_DEVICE_CLASS,
-    DOMAIN,
-    LOGGER,
+    SUBENTRY_SWITCH,
+    WHO_LIGHTING,
 )
-from .myhome_device import MyHOMEEntity
-from .gateway import MyHOMEGatewayHandler
+from .coordinator import MyHOMEGatewayCoordinator
+from .entity import MyHOMEEntity
+
+_LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    if PLATFORM not in hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS]:
-        return True
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up MyHOME switches from config entry subentries."""
+    coordinator: MyHOMEGatewayCoordinator = entry.runtime_data
 
-    _switches = []
-    _configured_switches = hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM]
+    entities = []
+    for subentry_id, subentry in entry.subentries.items():
+        if subentry.subentry_type == SUBENTRY_SWITCH:
+            entities.append(
+                MyHOMESwitch(coordinator, entry, subentry_id, subentry.data)
+            )
 
-    for _switch in _configured_switches.keys():
-        _switch = MyHOMESwitch(
-            hass=hass,
-            device_id=_switch,
-            who=_configured_switches[_switch][CONF_WHO],
-            where=_configured_switches[_switch][CONF_WHERE],
-            icon=_configured_switches[_switch][CONF_ICON],
-            icon_on=_configured_switches[_switch][CONF_ICON_ON],
-            interface=_configured_switches[_switch][CONF_BUS_INTERFACE] if CONF_BUS_INTERFACE in _configured_switches[_switch] else None,
-            name=_configured_switches[_switch][CONF_NAME],
-            entity_name=_configured_switches[_switch][CONF_ENTITY_NAME],
-            device_class=_configured_switches[_switch][CONF_DEVICE_CLASS],
-            manufacturer=_configured_switches[_switch][CONF_MANUFACTURER],
-            model=_configured_switches[_switch][CONF_DEVICE_MODEL],
-            gateway=hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_ENTITY],
-        )
-        _switches.append(_switch)
-
-    async_add_entities(_switches)
-
-
-async def async_unload_entry(hass, config_entry):
-    if PLATFORM not in hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS]:
-        return True
-
-    _configured_switches = hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM]
-
-    for _switch in _configured_switches.keys():
-        del hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM][_switch]
+    async_add_entities(entities)
 
 
 class MyHOMESwitch(MyHOMEEntity, SwitchEntity):
-    def __init__(
-        self,
-        hass,
-        name: str,
-        entity_name: str,
-        icon: str,
-        icon_on: str,
-        device_id: str,
-        who: str,
-        where: str,
-        interface: str,
-        device_class: str,
-        manufacturer: str,
-        model: str,
-        gateway: MyHOMEGatewayHandler,
-    ):
-        super().__init__(
-            hass=hass,
-            name=name,
-            platform=PLATFORM,
-            device_id=device_id,
-            who=who,
-            where=where,
-            manufacturer=manufacturer,
-            model=model,
-            gateway=gateway,
-        )
+    """Representation of a MyHOME switch."""
 
-        self._attr_name = entity_name
-
-        self._interface = interface
-        self._full_where = f"{self._where}#4#{self._interface}" if self._interface is not None else self._where
-
-        self._attr_extra_state_attributes = {
-            "A": where[: len(where) // 2],
-            "PL": where[len(where) // 2 :],
-        }
-        if self._interface is not None:
-            self._attr_extra_state_attributes["Int"] = self._interface
-
-        self._attr_device_class = SwitchDeviceClass.OUTLET if device_class.lower() == "outlet" else SwitchDeviceClass.SWITCH
-
-        self._on_icon = icon_on
-        self._off_icon = icon
-
-        if self._off_icon is not None:
-            self._attr_icon = self._off_icon
-
-        self._attr_is_on = None
-
-    async def async_update(self):
-        """Update the entity.
-
-        Only used by the generic entity update service.
-        """
-        await self._gateway_handler.send_status_request(OWNLightingCommand.status(self._where))
-
-    async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
-        """Turn the device on."""
-        await self._gateway_handler.send(OWNLightingCommand.switch_on(self._full_where))
-
-    async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
-        """Turn the device off."""
-        await self._gateway_handler.send(OWNLightingCommand.switch_off(self._full_where))
-
-    def handle_event(self, message: OWNLightingEvent):
-        """Handle an event message."""
-        if self._attr_device_class == SwitchDeviceClass.SWITCH:
-            LOGGER.info(
-                "%s %s",
-                self._gateway_handler.log_id,
-                message.human_readable_log.replace("Light", "Switch"),
-            )
-        elif self._attr_device_class == SwitchDeviceClass.OUTLET:
-            LOGGER.info(
-                "%s %s",
-                self._gateway_handler.log_id,
-                message.human_readable_log.replace("Light", "Outlet"),
-            )
+    def __init__(self, coordinator, entry, subentry_id, data) -> None:
+        super().__init__(coordinator, entry, subentry_id, data)
+        self._attr_is_on = False
+        dc = str(data.get(CONF_DEVICE_CLASS, "outlet"))
+        if dc == "outlet":
+            self._attr_device_class = SwitchDeviceClass.OUTLET
         else:
-            LOGGER.info(
-                "%s %s",
-                self._gateway_handler.log_id,
-                message.human_readable_log,
-            )
-        self._attr_is_on = message.is_on
-        if self._off_icon is not None and self._on_icon is not None:
-            self._attr_icon = self._on_icon if self._attr_is_on else self._off_icon
-        self.async_schedule_update_ha_state()
+            self._attr_device_class = SwitchDeviceClass.SWITCH
+
+    def _get_who(self) -> int:
+        return WHO_LIGHTING
+
+    async def _async_request_initial_state(self) -> None:
+        message = await self._coordinator.async_request_state(
+            WHO_LIGHTING, self._where
+        )
+        if message:
+            what = str(getattr(message, "what", ""))
+            self._attr_is_on = what == "1"
+
+    @callback
+    def _handle_event(self, message) -> None:
+        what = str(getattr(message, "what", ""))
+        self._attr_is_on = what == "1"
+        self.async_write_ha_state()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        await self._coordinator.async_send_message(
+            f"*1*1*{self._where}##"
+        )
+        self._attr_is_on = True
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        await self._coordinator.async_send_message(
+            f"*1*0*{self._where}##"
+        )
+        self._attr_is_on = False
+        self.async_write_ha_state()
