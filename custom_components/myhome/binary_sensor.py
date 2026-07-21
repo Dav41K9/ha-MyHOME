604343f3fb905c4cfe4b008651091aac8cede927
Dav41K9 <d.quattrone@hotmail.com>
2026-07-21 14:50:58 +0200

    Update binary_sensor.py

--- custom_components/myhome/binary_sensor.py
+++ custom_components/myhome/binary_sensor.py
@@ -1,336 +1,84 @@
-"""Support for MyHome binary sensors (dry contacts and motion sensors)."""
-from datetime import datetime, timedelta, timezone
+"""Binary sensor platform for BTicino MyHOME."""
+
+from __future__ import annotations
+
+import logging
+
 from homeassistant.components.binary_sensor import (
-    DOMAIN as PLATFORM,
     BinarySensorDeviceClass,
     BinarySensorEntity,
 )
-from homeassistant.const import (
-    CONF_NAME,
-    CONF_MAC,
-    CONF_ENTITIES,
-    STATE_ON,
-)
-from homeassistant.helpers.restore_state import RestoreEntity
-
-from OWNd.message import (
-    OWNDryContactEvent,
-    OWNDryContactCommand,
-    OWNLightingCommand,
-    MESSAGE_TYPE_MOTION,
-    MESSAGE_TYPE_PIR_SENSITIVITY,
-    MESSAGE_TYPE_MOTION_TIMEOUT,
-    OWNLightingEvent,
-)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import (
-    CONF_PLATFORMS,
-    CONF_ENTITY,
-    CONF_ENTITY_NAME,
-    CONF_WHO,
-    CONF_WHERE,
-    CONF_MANUFACTURER,
-    CONF_DEVICE_MODEL,
     CONF_DEVICE_CLASS,
-    CONF_INVERTED,
-    DOMAIN,
-    LOGGER,
+    CONF_OFF_VALUE,
+    CONF_ON_VALUE,
+    CONF_WHO,
+    SUBENTRY_BINARY_SENSOR,
+    WHO_LIGHTING,
 )
-from .myhome_device import MyHOMEEntity
-from .gateway import MyHOMEGatewayHandler
+from .coordinator import MyHOMEGatewayCoordinator
+from .entity import MyHOMEEntity
 
-SCAN_INTERVAL = timedelta(seconds=5)
-PIR_SENSITIVITY = ["low", "medium", "high", "very high"]
+_LOGGER = logging.getLogger(__name__)
+
+VALID_DEVICE_CLASSES = {dc.value for dc in BinarySensorDeviceClass}
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    if PLATFORM not in hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS]:
-        return True
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up MyHOME binary sensors from config entry subentries."""
+    coordinator: MyHOMEGatewayCoordinator = entry.runtime_data
 
-    _binary_sensors = []
-    _configured_binary_sensors = hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM]
-
-    for _binary_sensor in _configured_binary_sensors.keys():
-        _who = int(_configured_binary_sensors[_binary_sensor][CONF_WHO])
-        _device_class = _configured_binary_sensors[_binary_sensor][CONF_DEVICE_CLASS]
-        if _who == 25:
-            _binary_sensor = MyHOMEDryContact(
-                hass=hass,
-                device_id=_binary_sensor,
-                who=_configured_binary_sensors[_binary_sensor][CONF_WHO],
-                where=_configured_binary_sensors[_binary_sensor][CONF_WHERE],
-                name=_configured_binary_sensors[_binary_sensor][CONF_NAME],
-                entity_name=_configured_binary_sensors[_binary_sensor][CONF_ENTITY_NAME],
-                inverted=_configured_binary_sensors[_binary_sensor][CONF_INVERTED],
-                device_class=_device_class,
-                manufacturer=_configured_binary_sensors[_binary_sensor][CONF_MANUFACTURER],
-                model=_configured_binary_sensors[_binary_sensor][CONF_DEVICE_MODEL],
-                gateway=hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_ENTITY],
+    entities = []
+    for subentry_id, subentry in entry.subentries.items():
+        if subentry.subentry_type == SUBENTRY_BINARY_SENSOR:
+            entities.append(
+                MyHOMEBinarySensor(coordinator, entry, subentry_id, subentry.data)
             )
-            _binary_sensors.append(_binary_sensor)
-        elif _who == 9:
-            _binary_sensor = MyHOMEAuxiliary(
-                hass=hass,
-                device_id=_binary_sensor,
-                who=_configured_binary_sensors[_binary_sensor][CONF_WHO],
-                where=_configured_binary_sensors[_binary_sensor][CONF_WHERE],
-                name=_configured_binary_sensors[_binary_sensor][CONF_NAME],
-                entity_name=_configured_binary_sensors[_binary_sensor][CONF_ENTITY_NAME],
-                inverted=_configured_binary_sensors[_binary_sensor][CONF_INVERTED],
-                device_class=_device_class,
-                manufacturer=_configured_binary_sensors[_binary_sensor][CONF_MANUFACTURER],
-                model=_configured_binary_sensors[_binary_sensor][CONF_DEVICE_MODEL],
-                gateway=hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_ENTITY],
-            )
-            _binary_sensors.append(_binary_sensor)
-        elif _who == 1 and _device_class == BinarySensorDeviceClass.MOTION:
-            _binary_sensor = MyHOMEMotionSensor(
-                hass=hass,
-                device_id=_binary_sensor,
-                who=_configured_binary_sensors[_binary_sensor][CONF_WHO],
-                where=_configured_binary_sensors[_binary_sensor][CONF_WHERE],
-                name=_configured_binary_sensors[_binary_sensor][CONF_NAME],
-                entity_name=_configured_binary_sensors[_binary_sensor][CONF_ENTITY_NAME],
-                inverted=_configured_binary_sensors[_binary_sensor][CONF_INVERTED],
-                device_class=_device_class,
-                manufacturer=_configured_binary_sensors[_binary_sensor][CONF_MANUFACTURER],
-                model=_configured_binary_sensors[_binary_sensor][CONF_DEVICE_MODEL],
-                gateway=hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_ENTITY],
-            )
-            _binary_sensors.append(_binary_sensor)
 
-    async_add_entities(_binary_sensors)
+    async_add_entities(entities)
 
 
-async def async_unload_entry(hass, config_entry):
-    if PLATFORM not in hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS]:
-        return True
+class MyHOMEBinarySensor(MyHOMEEntity, BinarySensorEntity):
+    """Representation of a MyHOME binary sensor (motion, door, window, etc.)."""
 
-    _configured_binary_sensors = hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM]
-
-    for _binary_sensor in _configured_binary_sensors.keys():
-        del hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS][PLATFORM][_binary_sensor]
-
-
-class MyHOMEDryContact(MyHOMEEntity, BinarySensorEntity):
-    def __init__(
-        self,
-        hass,
-        name: str,
-        entity_name: str,
-        device_id: str,
-        who: str,
-        where: str,
-        inverted: bool,
-        device_class: str,
-        manufacturer: str,
-        model: str,
-        gateway: MyHOMEGatewayHandler,
-    ):
-        super().__init__(
-            hass=hass,
-            name=name,
-            platform=PLATFORM,
-            device_id=device_id,
-            who=who,
-            where=where,
-            manufacturer=manufacturer,
-            model=model,
-            gateway=gateway,
-        )
-
-        self._inverted = inverted
-
-        self._attr_device_class = device_class
-        self._attr_name = entity_name if entity_name else self._attr_device_class.replace("_", " ").capitalize()
-
-        self._attr_unique_id = f"{gateway.mac}-{self._device_id}-{self._attr_device_class}"
-
+    def __init__(self, coordinator, entry, subentry_id, data) -> None:
+        super().__init__(coordinator, entry, subentry_id, data)
+        self._who: int = int(data.get(CONF_WHO, WHO_LIGHTING))
+        self._on_value: str = str(data.get(CONF_ON_VALUE, "1"))
+        self._off_value: str = str(data.get(CONF_OFF_VALUE, "0"))
         self._attr_is_on = False
-        self._attr_extra_state_attributes = {"Sensor": f"({self._where[0]}){self._where[1:]}"}
 
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class] = self
-        await self.async_update()
+        dc = str(data.get(CONF_DEVICE_CLASS, "motion"))
+        if dc in VALID_DEVICE_CLASSES:
+            self._attr_device_class = BinarySensorDeviceClass(dc)
 
-    async def async_will_remove_from_hass(self):
-        """When entity is removed from hass."""
-        if self._attr_device_class in self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES]:
-            del self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class]
+    def _get_who(self) -> int:
+        return self._who
 
-    async def async_update(self):
-        """Update the entity.
-
-        Only used by the generic entity update service.
-        """
-        await self._gateway_handler.send_status_request(OWNDryContactCommand.status(self._where))
-
-    def handle_event(self, message: OWNDryContactEvent):
-        """Handle an event message."""
-        LOGGER.info(
-            "%s %s",
-            self._gateway_handler.log_id,
-            message.human_readable_log,
+    async def _async_request_initial_state(self) -> None:
+        message = await self._coordinator.async_request_state(
+            self._who, self._where
         )
-        self._attr_is_on = message.is_on != self._inverted
-        self.async_schedule_update_ha_state()
+        if message:
+            self._parse_state(message)
 
-
-class MyHOMEAuxiliary(MyHOMEEntity, BinarySensorEntity):
-    def __init__(
-        self,
-        hass,
-        name: str,
-        entity_name: str,
-        device_id: str,
-        who: str,
-        where: str,
-        inverted: bool,
-        device_class: str,
-        manufacturer: str,
-        model: str,
-        gateway: MyHOMEGatewayHandler,
-    ):
-        super().__init__(
-            hass=hass,
-            name=name,
-            platform=PLATFORM,
-            device_id=device_id,
-            who=who,
-            where=where,
-            manufacturer=manufacturer,
-            model=model,
-            gateway=gateway,
-        )
-
-        self._inverted = inverted
-
-        self._attr_device_class = device_class
-        self._attr_name = entity_name if entity_name else self._attr_device_class.replace("_", " ").capitalize()
-
-        self._attr_unique_id = f"{gateway.mac}-{self._device_id}-{self._attr_device_class}"
-
-        self._attr_is_on = False
-        self._attr_extra_state_attributes = {"Auxiliary channel": self._where}
-
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class] = self
-        await self.async_update()
-
-    async def async_will_remove_from_hass(self):
-        """When entity is removed from hass."""
-        if self._attr_device_class in self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES]:
-            del self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class]
-
-    async def async_update(self):
-        """AUX sensors are read only and cannot be queried, no async_update implementation."""
-
-    def handle_event(self, message: OWNDryContactEvent):
-        """Handle an event message."""
-        LOGGER.info(
-            "%s %s",
-            self._gateway_handler.log_id,
-            message.human_readable_log,
-        )
-        self._attr_is_on = message.is_on != self._inverted
-        self.async_schedule_update_ha_state()
-
-
-class MyHOMEMotionSensor(MyHOMEEntity, BinarySensorEntity, RestoreEntity):
-    def __init__(
-        self,
-        hass,
-        name: str,
-        entity_name: str,
-        device_id: str,
-        who: str,
-        where: str,
-        inverted: bool,
-        device_class: str,
-        manufacturer: str,
-        model: str,
-        gateway: MyHOMEGatewayHandler,
-    ):
-        super().__init__(
-            hass=hass,
-            name=name,
-            platform=PLATFORM,
-            device_id=device_id,
-            who=who,
-            where=where,
-            manufacturer=manufacturer,
-            model=model,
-            gateway=gateway,
-        )
-
-        self._inverted = inverted
-        self._attr_force_update = False
-        self._last_updated = None
-        self._timeout = timedelta(seconds=315)
-
-        self._attr_device_class = device_class
-        self._attr_name = entity_name if entity_name else self._attr_device_class.replace("_", " ").capitalize()
-
-        self._attr_unique_id = f"{gateway.mac}-{self._device_id}-{self._attr_device_class}"
-        self._attr_should_poll = True
-        self._attr_is_on = None
-        self._attr_extra_state_attributes = {
-            "A": where[: len(where) // 2],
-            "PL": where[len(where) // 2 :],
-            "Timeout": self._timeout.total_seconds(),
-            "Sensitivity": PIR_SENSITIVITY[1],
-        }
-
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class] = self
-        await self._gateway_handler.send_status_request(OWNLightingCommand.get_pir_sensitivity(self._where))
-        await self._gateway_handler.send_status_request(OWNLightingCommand.get_motion_timeout(self._where))
-        state = await self.async_get_last_state()
-        if state:
-            self._attr_is_on = state.state == STATE_ON
-            self._last_updated = state.last_updated
-        await self.async_update()
-
-    async def async_will_remove_from_hass(self):
-        """When entity is removed from hass."""
-        if self._attr_device_class in self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES]:
-            del self._hass.data[DOMAIN][self._gateway_handler.mac][CONF_PLATFORMS][self._platform][self._device_id][CONF_ENTITIES][self._attr_device_class]
-
-    async def async_update(self):
-        """Update the entity.
-
-        Only used by the generic entity update service.
-        """
-        if self._attr_is_on and self._last_updated and self._last_updated + self._timeout < datetime.now(timezone.utc):
-            self._attr_is_on = False
-            self._last_updated = datetime.now(timezone.utc)
-            self.async_schedule_update_ha_state()
-
-    def handle_event(self, message: OWNLightingEvent):
-        """Handle an event message."""
-        if message.message_type not in [
-            MESSAGE_TYPE_MOTION,
-            MESSAGE_TYPE_MOTION_TIMEOUT,
-            MESSAGE_TYPE_PIR_SENSITIVITY,
-        ]:
-            return True
-
-        LOGGER.info(
-            "%s %s",
-            self._gateway_handler.log_id,
-            message.human_readable_log,
-        )
-        if message.message_type == MESSAGE_TYPE_MOTION and message.motion:
-            self._attr_is_on = message.motion != self._inverted
-        elif message.message_type == MESSAGE_TYPE_MOTION_TIMEOUT:
-            self._timeout = message.motion_timeout + timedelta(seconds=15)
-            self._attr_extra_state_attributes["Timeout"] = self._timeout.total_seconds()
-        elif message.message_type == MESSAGE_TYPE_PIR_SENSITIVITY:
-            self._attr_extra_state_attributes["Sensitivity"] = PIR_SENSITIVITY[message.pir_sensitivity]
-        self._last_updated = datetime.now(timezone.utc)
-        self._attr_force_update = True
+    @callback
+    def _handle_event(self, message) -> None:
+        self._parse_state(message)
         self.async_write_ha_state()
-        self._attr_force_update = False
+
+    @callback
+    def _parse_state(self, message) -> None:
+        what = str(getattr(message, "what", ""))
+        if what == self._on_value:
+            self._attr_is_on = True
+        elif what == self._off_value:
+            self._attr_is_on = False
