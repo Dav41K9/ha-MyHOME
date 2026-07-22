d4e29e77bf4d4fa01cacb4b1403feb4c277d4d54
Dav41K9 <d.quattrone@hotmail.com>
2026-07-23 01:04:29 +0200

    Update binary_sensor.py

--- custom_components/myhome/binary_sensor.py
+++ custom_components/myhome/binary_sensor.py
@@ -1,84 +1,96 @@
-"""Binary sensor platform for BTicino MyHOME."""
-
+"""Dry contacts (WHO 25), motion (WHO 1), aux (WHO 9)."""
 from __future__ import annotations
 
-import logging
-
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
+from OWNd.message import (
+    MESSAGE_TYPE_MOTION,
+    MESSAGE_TYPE_MOTION_TIMEOUT,
+    MESSAGE_TYPE_PIR_SENSITIVITY,
+    OWNDryContactCommand,
+    OWNDryContactEvent,
+    OWNLightingCommand,
+    OWNLightingEvent,
 )
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
     CONF_DEVICE_CLASS,
-    CONF_OFF_VALUE,
-    CONF_ON_VALUE,
+    CONF_INVERTED,
+    CONF_MANUFACTURER,
+    CONF_MODEL,
+    CONF_NAME,
+    CONF_WHERE,
     CONF_WHO,
     SUBENTRY_BINARY_SENSOR,
-    WHO_LIGHTING,
 )
-from .coordinator import MyHOMEGatewayCoordinator
 from .entity import MyHOMEEntity
 
-_LOGGER = logging.getLogger(__name__)
 
-VALID_DEVICE_CLASSES = {dc.value for dc in BinarySensorDeviceClass}
+def _dc(value: str) -> BinarySensorDeviceClass | None:
+    return getattr(BinarySensorDeviceClass, value.upper(), None) if value else None
 
 
 async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddConfigEntryEntitiesCallback,
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up MyHOME binary sensors from config entry subentries."""
-    coordinator: MyHOMEGatewayCoordinator = entry.runtime_data
+    coord = entry.runtime_data
+    out = []
+    for sub in entry.subentries.values():
+        if sub.subentry_type != SUBENTRY_BINARY_SENSOR:
+            continue
+        who = int(sub.data.get(CONF_WHO, 25))
+        if who == 1:
+            out.append(MyHOMEMotion(coord, sub.subentry_id, sub.data))
+        elif who == 9:
+            out.append(MyHOMEAux(coord, sub.subentry_id, sub.data))
+        else:
+            out.append(MyHOMEDryContact(coord, sub.subentry_id, sub.data))
+    async_add_entities(out)
 
-    entities = []
-    for subentry_id, subentry in entry.subentries.items():
-        if subentry.subentry_type == SUBENTRY_BINARY_SENSOR:
-            entities.append(
-                MyHOMEBinarySensor(coordinator, entry, subentry_id, subentry.data)
-            )
 
-    async_add_entities(entities)
-
-
-class MyHOMEBinarySensor(MyHOMEEntity, BinarySensorEntity):
-    """Representation of a MyHOME binary sensor (motion, door, window, etc.)."""
-
-    def __init__(self, coordinator, entry, subentry_id, data) -> None:
-        super().__init__(coordinator, entry, subentry_id, data)
-        self._who: int = int(data.get(CONF_WHO, WHO_LIGHTING))
-        self._on_value: str = str(data.get(CONF_ON_VALUE, "1"))
-        self._off_value: str = str(data.get(CONF_OFF_VALUE, "0"))
+class _BaseBinary(MyHOMEEntity, BinarySensorEntity):
+    def __init__(self, coordinator, subentry_id, data, who):
+        super().__init__(coordinator, subentry_id, who, data[CONF_WHERE], data[CONF_NAME],
+                         data.get(CONF_MANUFACTURER, ""), data.get(CONF_MODEL, ""))
+        self._inverted = bool(data.get(CONF_INVERTED, False))
+        self._attr_device_class = _dc(data.get(CONF_DEVICE_CLASS, ""))
         self._attr_is_on = False
 
-        dc = str(data.get(CONF_DEVICE_CLASS, "motion"))
-        if dc in VALID_DEVICE_CLASSES:
-            self._attr_device_class = BinarySensorDeviceClass(dc)
 
-    def _get_who(self) -> int:
-        return self._who
+class MyHOMEDryContact(_BaseBinary):
+    def __init__(self, c, s, d):
+        super().__init__(c, s, d, 25)
 
-    async def _async_request_initial_state(self) -> None:
-        message = await self._coordinator.async_request_state(
-            self._who, self._where
-        )
-        if message:
-            self._parse_state(message)
+    async def async_request_initial_state(self) -> None:
+        await self._coordinator.send_status_request(OWNDryContactCommand.status(self._where))
 
-    @callback
-    def _handle_event(self, message) -> None:
-        self._parse_state(message)
+    def handle_event(self, message: OWNDryContactEvent) -> None:
+        self._attr_is_on = message.is_on != self._inverted
         self.async_write_ha_state()
 
-    @callback
-    def _parse_state(self, message) -> None:
-        what = str(getattr(message, "what", ""))
-        if what == self._on_value:
-            self._attr_is_on = True
-        elif what == self._off_value:
-            self._attr_is_on = False
+
+class MyHOMEAux(_BaseBinary):
+    def __init__(self, c, s, d):
+        super().__init__(c, s, d, 9)
+
+    def handle_event(self, message: OWNDryContactEvent) -> None:
+        self._attr_is_on = message.is_on != self._inverted
+        self.async_write_ha_state()
+
+
+class MyHOMEMotion(_BaseBinary):
+    def __init__(self, c, s, d):
+        super().__init__(c, s, d, 1)
+        self._attr_device_class = BinarySensorDeviceClass.MOTION
+
+    async def async_request_initial_state(self) -> None:
+        await self._coordinator.send_status_request(OWNLightingCommand.get_pir_sensitivity(self._where))
+
+    def handle_event(self, message: OWNLightingEvent) -> None:
+        if message.message_type == MESSAGE_TYPE_MOTION:
+            self._attr_is_on = bool(message.motion) != self._inverted
+            self.async_write_ha_state()
+        elif message.message_type in (MESSAGE_TYPE_MOTION_TIMEOUT, MESSAGE_TYPE_PIR_SENSITIVITY):
+            self.async_write_ha_state()
